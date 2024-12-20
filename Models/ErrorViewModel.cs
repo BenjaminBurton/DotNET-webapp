d4772d9742040524d3dc238e7a45ff2f90378b12
Mr. Burton <benjaminlburton@gmail.com>
2024-12-20 18:45:12 -0500

    created azure web app dotNET

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace azWebAppPractice2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
